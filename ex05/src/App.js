8d17d462f52be5daaa9710bf54c11f747806c637
amir-pasalic <amirpasalic144@gmail.com>
2021-03-30 18:02:39 +0200

    Assignment done !

---
+++ ex05/src/App.js
@@ -0,0 +1,11 @@
+//import logo from './logo.svg';
+import './App.css';
+import SimpleForm from './SimpleForm'
+function App() {
+  return (
+    <div className="App">
+      <SimpleForm />
+    </div>
+  );
+}
+export default App;
